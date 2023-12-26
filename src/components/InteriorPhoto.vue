d1d6f63ce1c1bf2d7be26e5fb0a421d2fe5933bf
DavidGyulinyan <davidgyulinyan@gmail.com>
2023-12-26 14:58:41 +0400

    the interior photo turned to parralax effect

--- src/components/InteriorPhoto.vue
+++ src/components/InteriorPhoto.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="w-full flex justify-center mt-32">
-    <img
-         :src="image"
-         alt="interior photo"
-         class="w-3/5 h-3/5"
-    >
-  </div>
+  <section class="w-full flex justify-center items-center">
+    <div
+        class="w-full mt-64 h-[1000px] overflow-y-scroll no-scrollbar bg-cover bg-fixed bg-center bg-no-repeat shadow-lg"
+        :style="{ backgroundImage: 'url(' + image + ')' }">
+    </div>
+  </section>
 </template>
 
 <script>
-import image from "../assets/testImagesFolder/109400833_144990773884210_8771481519331202718_n.jpg"
+import image from "../assets/testImagesFolder/323352287_5936254923063030_4971183037352376912_n.jpg"
 
 export default {
   data: function () {
