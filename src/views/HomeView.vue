1b774af3e1152dd0c4491e984480f7cbfa7cec86
David Gyulinyan <davidgyulinyan@gmail.com>
2024-01-19 21:18:04 +0400

    routing is done :D

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,15 @@
 <template>
   <main class="text-white relative">
-    <AppHeader/>
     <SliderAppSection/>
     <AppAboutUsHome/>
     <InteriorPhoto/>
     <ProjectTypeSection/>
     <AppTestimonials/>
-    <AppFooter/>
   </main>
 </template>
 <script setup>
 import SliderAppSection from '@/components/SliderAppSection.vue'
 import AppTestimonials from '@/components/AppTestimonials.vue'
-import AppFooter from '@/components/AppFooter.vue'
-import AppHeader from '@/components/AppHeader.vue'
 import ProjectTypeSection from '@/components/ProjectTypeSection.vue'
 import AppAboutUsHome from '@/components/AppAboutUsHome.vue'
 import InteriorPhoto from '@/components/InteriorPhoto.vue'
